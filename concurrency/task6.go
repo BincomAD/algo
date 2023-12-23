f7a57dcc2ffee3ce9fd63cc1b5a56a58e4c507ed
Aleksandr Dmitriev <100017314+BincomAD@users.noreply.github.com>
2023-12-23 19:05:41 +0300

    Create task6.go

---
+++ concurrency/task6.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		select {
+		case val := <-ch:
+			fmt.Println(val)
+		default:
+			fmt.Println("default")
+		}
+	}()
+
+	time.Sleep(time.Second)
+	fmt.Println(runtime.NumGoroutine())
+}
